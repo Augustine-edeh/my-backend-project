65972262106fcf4156a6fccf3f2399400cd6755c
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-10-13 08:11:52 +0100

    fix: fix bug on  credential validation logic for `username` and `password`

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/register", (req, res) => {
   if (!req.body.username) errors.push("You must a username.");
   if (req.body.username && req.body.username.length < 3)
     errors.push("Username must be at least 3 characters.");
-  if (req.body.username && req.body.username.length < 10)
+  if (req.body.username && req.body.username.length > 10)
     errors.push("Username can not exceed 10 characters.");
   if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))
     errors.push("Username can only contain letters and numbers.");
@@ -58,7 +58,7 @@ app.post("/register", (req, res) => {
   if (!req.body.password) errors.push("You must a password.");
   if (req.body.password && req.body.password.length < 3)
     errors.push("Password must be at least 12 characters.");
-  if (req.body.password && req.body.password.length < 70)
+  if (req.body.password && req.body.password.length > 70)
     errors.push("Password can not exceed 70 characters.");
 
   if (errors.length) {
